843e80532c1bb101f167bdfc29d6fe4c26b89795
Davide_Acquaviva <davacquaviva@gmail.com>
2023-09-21 12:34:07 +0000

    Card(   child: ListTile(     leading: Icon(        ...     ),   title: Text(        ...     ), ),

--- lib/main.dart
+++ lib/main.dart
@@ -63,97 +63,17 @@ class slwNomeWidget extends StatelessWidget {
                   letterSpacing: 2.5, //spaziatura tra le lettere
                 ),
               ),
-              SizedBox(height: 50),
+              SizedBox(height: 40),
               Card(
-                //PHONE NUMBER
-                color: Colors.white,
-                //padding: EdgeInsets.all(10),
-                margin: EdgeInsets.only(left: 15, right: 15),
-                //width: 280,
-                /*
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 25,
-                ),
-                */
-
-                //height: 55,
-                child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Row(children: <Widget>[
-                    Icon(
-                      Icons.phone_android_rounded,
-                      color: Colors.green,
-                      size: 40.0,
-                      semanticLabel: 'Text to announce in accessibility modes',
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text(
-                      '+39 349 4223607',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'SourceSans3-Regular',
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
-              SizedBox(height: 20),
-              Card(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                //padding: EdgeInsets.all(10),
-                color: Colors.white,
-                //height: 60,
-                //width: 320,
-                child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.mail,
-                        color: Colors.teal.shade300,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        'davacquaviva@gmail.com',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-
-              /*
-              Container(//E-MAIL ADDRESS
-                color: Colors.white,
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.only(left: 15),
-                width: 280,
-                
-
-                height: 70,
-                child: Row(children: <Widget>[
-                  Icon(
-                    Icons.mail,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android_rounded,
                     color: Colors.green,
                     size: 40.0,
                     semanticLabel: 'Text to announce in accessibility modes',
                   ),
-                  Container(
-                    width: 20,
-                  ),
-                  Text(
-                    'info@email.it',
+                  title: Text(
+                    '+39 349 4223607',
                     style: TextStyle(
                       color: Colors.teal.shade900,
                       fontFamily: 'SourceSans3-Regular',
@@ -161,9 +81,28 @@ class slwNomeWidget extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ]),
+                ),
               ),
-              */
+              SizedBox(height: 30),
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.green,
+                    size: 40.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  title: Text(
+                    'davacquaviva@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans3-Regular',
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
